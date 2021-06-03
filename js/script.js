b73d88f9ec8e4b7380c010f7d048f7517050da43
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-03 23:00:42 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// 1. chiedi all’utente il cognome
+// 2. inseriscilo in un array dove sono presenti già altri cognomi: 
+//‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione “umana” (partendo da 1) della lista in cui il nuovo utente si trova
+
+var output = document.getElementsById('output');
+var surnameClient = parseInt(prompt('Inserire il Suo cognome'));
+var listSurnames=['Bianchi','Neri', 'Rossi', 'Verdi', 'Gialli'];
+
+listSurnames.push(surnameClient);
+
+while (){
+    
+}
