cdd8d064cf7844b052d2940d77e7b658ae176daa
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-13 17:01:34 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -4,12 +4,19 @@
 // 3. stampa la lista ordinata alfabeticamente
 // 4. scrivi anche la posizione “umana” (partendo da 1) della lista in cui il nuovo utente si trova
 
-var output = document.getElementsById('output');
-var surnameClient = parseInt(prompt('Inserire il Suo cognome'));
+var output = document.getElementById('output');
+var surnameClient = prompt('Inserire il Suo cognome');
 var listSurnames=['Bianchi','Neri', 'Rossi', 'Verdi', 'Gialli'];
 
+surnameClient =surnameClient[0].toUpperCase()+surnameClient.slice(1);
 listSurnames.push(surnameClient);
 
-while (){
-    
-}
+document.getElementById('result').innerHTML = listSurnames;
+
+listSurnames.sort();
+document.getElementById('result').innerHTML += '<br>'+listSurnames;
+
+var position = listSurnames.indexOf(surnameClient)+1;
+document.getElementById('result').innerHTML+= '<br>' +'Your position is' + ' ' +position;
+
+
